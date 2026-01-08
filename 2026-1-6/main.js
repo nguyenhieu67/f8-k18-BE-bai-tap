45b8a5587cf4cef4f24f2257d67737a04deeac3b
nguyenhieu67 <149569751+nguyenhieu67@users.noreply.github.com>
2026-01-08 09:08:05 +0700

    Correct homework 2025-1-6

--- 2026-1-6/main.js
+++ 2026-1-6/main.js
@@ -8,64 +8,58 @@ function classifyTriangle(a, b, c) {
         typeof c !== "number" ||
         isNaN(c)
     )
-        return console.log("Please enter a valid number.");
-    if (a <= 0 || b <= 0 || c <= 0)
-        return console.log("The sides must be greater than 0.");
+        return "Please enter a valid number.";
+    if (a <= 0 || b <= 0 || c <= 0) return "The sides must be greater than 0.";
     if (a + b > c && a + c > b && b + c > a) {
         if (a === b && b === c) {
-            console.log("Equilateral triangle");
-            return;
+            return "Equilateral triangle";
         }
 
         if (a === b || a === c || b === c) {
-            console.log("Isosceles triangle");
-            return;
+            return "Isosceles triangle";
         }
 
         let a2 = a ** 2;
         let b2 = b ** 2;
         let c2 = c ** 2;
         if (a2 === b2 + c2 || b2 === a2 + c2 || c2 === b2 + a2) {
-            console.log("Right triangle");
-            return;
+            return "Right triangle";
         } else {
-            console.log("General triangle");
-            return;
+            return "General triangle";
         }
     } else {
-        console.log("Not a triangle");
-        return;
+        return "Not a triangle";
     }
 }
 
-classifyTriangle(2, 2, 2);
-classifyTriangle(3, 3, 5);
-classifyTriangle(3, 4, 5);
-classifyTriangle(3, 4, 2);
-classifyTriangle(2, 4, 2);
+console.log(classifyTriangle(2, 2, 2));
+console.log(classifyTriangle(3, 3, 5));
+console.log(classifyTriangle(3, 4, 5));
+console.log(classifyTriangle(3, 4, 2));
+console.log(classifyTriangle(2, 4, 2));
 
 // Task 2:
+// func check if it is an integer
+function isInteger(n) {
+    return n % 1 === 0;
+}
 
 function isPerfectSquares(a) {
     if (typeof a !== "number" || isNaN(a))
-        return console.log("Please enter a valid number.");
-    if (a < 0) return console.log(`${a} is not prefect squares`);
+        return "Please enter a valid number.";
+    if (a < 0) return `${a} is not prefect squares`;
 
     let squareRoot = a ** 0.5;
 
-    function isInteger(n) {
-        return n % 1 === 0;
-    }
-
-    if (isInteger(squareRoot)) {
-        return console.log(`${a} is prefect squares`);
+    if (!isInteger(squareRoot)) {
+        return false;
     } else {
-        return console.log(`${a} not prefect squares`);
+        return true;
     }
 }
 
-isPerfectSquares(1);
-isPerfectSquares(2);
-isPerfectSquares(4);
-isPerfectSquares(5);
-isPerfectSquares(9);
+console.log(isPerfectSquares(1));
+console.log(isPerfectSquares(2));
+console.log(isPerfectSquares(4));
+console.log(isPerfectSquares(5));
+console.log(isPerfectSquares(9));
